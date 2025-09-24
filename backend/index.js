5cdb88b16514aba257d4a5a827f1967d8a098eef
Inayath786 <134905331+Inayath786@users.noreply.github.com>
2025-09-24 15:23:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ const limiter=ratelimit({
 
 app.use(limiter)
 
-mongoose.connect("mongodb://localhost:27017/expenses",{
+mongoose.connect("mongodb+srv://user454:user454@expense-tracker.ektfild.mongodb.net/?retryWrites=true&w=majority&appName=expense-tracker",{
     useNewUrlParser:true
 }).then(()=>{
     console.log("Connected to mongodb")
